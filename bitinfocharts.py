a1dee028e047c70de001db09ae7746ffccdb08be
unknown <28350180+oceanboi1337@users.noreply.github.com.>
2024-02-29 08:09:43 +0100

    Goofball code

--- bitinfocharts.py
+++ bitinfocharts.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 import re, datetime
+import json
 
 def _request(url: str, timeout: float = 5, retries: int = 3) -> requests.Response:
     for retry in range(retries):
@@ -18,11 +19,22 @@ def _parse(str: str):
     str = re.sub('[\[\],\s]', str)
     return [x for x in re.split('[\'\"]', str) if x != '']
 
-def fetch(start_date, end_date, charts: list[str]) -> pd.DataFrame:
+def fetch(start_date, end_date, coins: list[str], charts: list[str]) -> pd.DataFrame:
     df = pd.DataFrame({'date': pd.date_range(start=start_date, end=end_date)})
 
+    prefix = '-'.join(coins)
+
+    charts_map = {
+        'hashrate': f'{prefix}-hashrate',
+        'tweets': f'tweets-{prefix}',
+        'transactions': f'transactions-{prefix}',
+        'marketcap': f'marketcap-{prefix}',
+        'difficulty': f'difficulty-{prefix}'
+    }
+
     for chart in charts:
-        data = _request(f'https://bitinfocharts.com/comparison/{chart}.html#alltime')
+        endpoint = charts_map[chart]
+        data = _request(f'https://bitinfocharts.com/comparison/{endpoint}.html#alltime')
         if not data:
             continue
 
@@ -43,23 +55,21 @@ def fetch(start_date, end_date, charts: list[str]) -> pd.DataFrame:
         if not rows:
             continue
 
-        dates, values = np.array(rows[::2]), np.array(rows[1::2])
-        values = [value.astype(np.float_) if value != 'null' else 0 for value in values]
+        # Goofy ahh code
+        filtered_rows = {f'{k}_{chart}' if k != 'date' else k: [] for k in ['date'] + coins}
+        for i, row in enumerate(rows):
+            k = (['date'] + coins)[i % (len(coins) + 1)]
+            filtered_rows[f'{k}_{chart}' if k != 'date' else k].append(row)
 
-        col = ''
-        if 'hashrate' in chart: col = 'hashrate'
-        elif 'tweets' in chart: col = 'tweets'
+        tmp = pd.DataFrame.from_dict(filtered_rows)
+        tmp['date'] = pd.to_datetime(tmp['date'])
 
-        tmp = pd.DataFrame({'date': pd.date_range(start=str(dates[0]), end=str(dates[-1]), periods=len(values)), col: values})
         df = pd.merge(df, tmp, on='date', how='outer')
-
         df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
-
         df.fillna(0, inplace=True)
 
-        df.to_csv('datasets/bitinfo.csv')
-
-        return df
+    df.to_csv('datasets/bitinfo.csv')
+    return df
 
 if __name__ == '__main__':
-    fetch('2014-09-17', '2024-02-22', ['bitcoin-hashrate', 'tweets-btc'])
+    fetch('2014-09-17', '2024-02-22', ['btc', 'eth', 'ltc'], ['hashrate', 'tweets', 'transactions', 'marketcap', 'difficulty'])
